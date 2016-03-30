203e146265563821341d961a12acaadb6a74e795
standartStan <stas.zoria@gmail.com>
2016-03-30 19:18:08 +0300

    add beforeclass

--- src/test/java/com/main/GmailTest.java
+++ src/test/java/com/main/GmailTest.java
@@ -2,6 +2,7 @@ package com.main;
 
 import static com.codeborne.selenide.Condition.visible;
 import com.codeborne.selenide.Configuration;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static com.codeborne.selenide.Selenide.open;
 import static pages.Gmail.*;
@@ -9,10 +10,13 @@ import static pages.Gmail.*;
 
 public class GmailTest {
 
+    @BeforeClass
+    public void setTimeout(){
+        Configuration.timeout = 15000;
+    }
+
     @Test
     public void testGmailSendSearch(){
-
-        Configuration.timeout = 15000;
 
         open("https://mail.google.com/");
 
